4b476e5cb88778e4fcf3707dceb3657e4a82d1e9
RyosukeYashima <r.yashima@yds-net.com>
2024-04-12 18:40:10 +0900

    バリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        validates :first_name, :email,:last_name, :birthday, :kana_first_name, :kana_last_name, :nickname,  :email, :encrypted_password, presence: true
+        validates :email, uniqueness: true
+        validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+        validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).*\z/, message: "は半角英数字混合で入力してください" }
+        validates :password, confirmation: true
+        
+        has_many :items
+        has_many :shoppings       
+
 end
