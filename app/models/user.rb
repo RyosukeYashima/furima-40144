c747028b53b03b5f34739a91669c596d447f841d
RyosukeYashima <r.yashima@yds-net.com>
2024-04-13 16:20:51 +0900

    新規登録ログインパス

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,9 @@ class User < ApplicationRecord
         validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
         validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).*\z/, message: "は半角英数字混合で入力してください" }
         validates :password, confirmation: true
-        
+        validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+        validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+
         has_many :items
         has_many :shoppings       
 
