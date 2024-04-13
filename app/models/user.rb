5fbba98ef0bece315b6b7e48c59cef5cec81bf7e
RyosukeYashima <r.yashima@yds-net.com>
2024-04-13 21:49:50 +0900

    テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-        validates :first_name, :email,:last_name, :birthday, :kana_first_name, :kana_last_name, :nickname,  :email, :encrypted_password, presence: true
-        validates :email, uniqueness: true
-        validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-        validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).*\z/, message: "は半角英数字混合で入力してください" }
-        validates :password, confirmation: true
+        :recoverable, :rememberable, :validatable
+        validates :nickname, presence: true
+        #validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+        validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角文字で入力してください" }
+        validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角文字で入力してください" }
         validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
         validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+        validates :birthday, presence: true
+        validates :password,  format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).*\z/, message: "は半角英数字混合で入力してください" }
+        #validates :password, confirmation: true
 
         has_many :items
-        has_many :shoppings       
+        has_many :shoppings
 
 end
