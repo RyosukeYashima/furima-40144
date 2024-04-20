b655dfc3f6e301db6a814f6f8c7f79bdf3431dfd
RyosukeYashima <r.yashima@yds-net.com>
2024-04-20 18:01:09 +0900

    ActiveHashの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :days_to_ship,:shipping_fee_burden,:condition,:category,:shipping_from
+  belongs_to :user
+
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_burden_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_from_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_to_ship_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
 end
