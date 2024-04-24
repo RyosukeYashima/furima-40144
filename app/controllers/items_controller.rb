e8d82593c0cf08f78109f3b6baad2fbf5faf2b5d
RyosukeYashima <r.yashima@yds-net.com>
2024-04-24 18:24:08 +0900

    Active storage実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
+  def index
+    @item = Item.order("created_at DESC")
+  end
+
   def new
     @item = Item.new
   end
@@ -15,6 +19,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image,:name,:explanation,:price,:category_id,:condition_id,:shipping_fee_burden_id,:shipping_from_id,:days_to_ship_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :explanation, :price, :category_id, :condition_id, :shipping_fee_burden_id, :shipping_from_id, :days_to_ship_id).merge(user_id: current_user.id)
   end
+  
 end
