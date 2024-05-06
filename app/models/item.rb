70acef3510063c72368abeb644b217f20eb14124
RyosukeYashima <r.yashima@yds-net.com>
2024-05-06 20:30:18 +0900

    スネークキャメルケース変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :DaysToShip
-  belongs_to :ShippingFeeBurden
+  belongs_to :days_to_ship
+  belongs_to :shipping_fee_burden
   belongs_to :condition
   belongs_to :category
-  belongs_to :ShippingFrom
+  belongs_to :shipping_from
 
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
