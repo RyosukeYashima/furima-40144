4f3621cf7c4ff1c93110dd37b61f07535d47189d
RyosukeYashima <r.yashima@yds-net.com>
2024-05-06 13:12:27 +0900

    テストコード記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Commerce.product_name }
+    price { Faker::Number.between(from: 300, to: 9999999) }
+    explanation { Faker::Lorem.paragraph(sentence_count: 3) }
+    category_id { Category.all.sample } # ActiveHash モデルの範囲内からランダムに選択
+    condition_id { Condition.all.sample } # ActiveHash モデルの範囲内からランダムに選択
+    shipping_fee_burden_id { ShippingFeeBurden.all.sample } # ActiveHash モデルの範囲内からランダムに選択
+    shipping_from_id { ShippingFrom.all.sample } # ActiveHash モデルの範囲内からランダムに選択
+    days_to_ship_id { DaysToShip.all.sample } # ActiveHash モデルの範囲内からランダムに選択
+    image { Faker::Lorem.sentence }
+    association :user
   end
-end
+end 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,90 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)  # テスト用の商品を作成する
+  end
+
+  describe '商品出品機能' do
+    context 'バリデーションが正常に機能する場合' do
+      it '全ての必須項目を正しく入力した場合、商品が有効であること' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '必須項目が欠けている場合' do
+      it 'カテゴリーが欠けている場合、商品が無効であること' do
+        @item.category = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it '商品名が欠けている場合、商品が無効であること' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it '商品の説明が欠けている場合、商品が無効であること' do
+        @item.explanation = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+
+      it '商品画像が添付されていない場合、商品が無効であること' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'カテゴリーが選択肢の1以外の場合、商品が無効であること' do
+        @item.category_id = 2
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be 1")
+      end
+
+      it '商品の状態が選択肢の1以外の場合、商品が無効であること' do
+        @item.condition_id = 2
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be 1")
+      end
+
+      it '配送料の負担が選択肢の1以外の場合、商品が無効であること' do
+        @item.shipping_fee_burden_id = 2
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee burden must be 1")
+      end
+
+      it '発送元の地域が選択肢の1以外の場合、商品が無効であること' do
+        @item.shipping_from_id = 2
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping from must be 1")
+      end
+
+      it '発送までの日数が選択肢の1以外の場合、商品が無効であること' do
+        @item.days_to_ship_id = 2
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to ship must be 1")
+      end
+
+      it '価格が300未満の場合、商品が無効であること' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it '価格が9999999を超える場合、商品が無効であること' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+
+      it '価格が半角数値以外の場合、商品が無効であること' do
+        @item.price = 'abc'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
 end
+
